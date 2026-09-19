d05a98af2a7ccce3897c901cafdc472f8ad14509
silentl0tus <maxboyzaitsev1@yandex.ru>
2026-09-19 20:02:26 +0300

    fix: update bot version string to 1.2 in handlers and tests

--- hh-autoresponder/app/bot/handlers.py
+++ hh-autoresponder/app/bot/handlers.py
@@ -70,7 +70,7 @@ class ManualCoverLetter(StatesGroup):
 @admin_only
 async def cmd_start(message: Message, **kw):
     await message.answer(
-        "👋 <b>Job Hunter Bot v1.1</b>\n\n"
+        "👋 <b>Job Hunter Bot v1.2</b>\n\n"
         "Автоматический поиск вакансий и отклики на hh.ru\n"
         "Используй кнопки ниже 👇",
         parse_mode="HTML",
